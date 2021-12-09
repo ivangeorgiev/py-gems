72b50b7bb46581147d6daee710efbdc758dcad33
Ivan Georgiev <ivan.georgiev@gmail.com>
2021-12-09 18:54:52 +0200

    fix: failing test with Python 3.8

--- docs/knowing/patterns/scripts/plugin-entry-points/list_console_scripts.py
+++ docs/knowing/patterns/scripts/plugin-entry-points/list_console_scripts.py
@@ -2,4 +2,4 @@
 
 from importlib import metadata
 for ep in metadata.entry_points()['console_scripts']:
-    print(f"name='{ep.name}', value='{ep.value}', module='{ep.module}', attr='{ep.attr}'")
+    print(f"name='{ep.name}', value='{ep.value}'")
